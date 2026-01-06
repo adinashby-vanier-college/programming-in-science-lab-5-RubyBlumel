ef92d1a1845d1b9a5da82e37ef4a9a874f69893b
RubyBlumel <rkblumel@gmail.com>
2026-01-06 12:29:07 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,35 +1,44 @@
+# Example for n = 5:
+
+# *****
+# *   *
+# *   *
+# *   *
+# *****
 def hollow_square(n):
     if n == 1:
         square = "*"
-         return square
-    elif n == 2: 
-        square = ("**", "**")
-        for square in square:
-            return square
-        
+        return square
+   
     elif n == 3:
         square = ("***", "* *", "***")
         for square in square:
             return square
-    
-    elif n == 5 
-    square = ("*****", "*   *", "*   *", "*   *", "*****")
-    for square in square
-        return square
-    
     else:    
         x = "*"
-        y = " "
+        y = " " 
+        result2 = x * n 
+        mult = ((n - 2) * y) 
+        
+    for mult in mult:
+        mult = [n * mult]
 
-        print(x * n)
             
-        count = 2
-        while count <= n - 1:
-            print(x, ((n - 4) * y), x)
-            count += 1
+            
+            
+    result1 = f"{x}{mult}{x}"
+           
+            
+        
+    return f"{result2}\n{result1}\n{result2}"
+            
+            
+           
 
-        print(x * n)
+       
 
+a = hollow_square(5)
+print(a)
 
         
 
@@ -52,4 +61,25 @@ def hollow_square(n):
 # def centered_star_pyramid(n):
 #     return ""
 
-hollow_square(3)
+
+        
+
+# # 1
+# # 12
+# # 123
+# # 1234
+# def number_pattern(n):
+#     return ""
+
+# # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
+# def sum_of_natural_numbers(n):
+#     return ""
+
+# # Example for n = 4:
+# #    *
+# #   ***
+# #  *****
+# # *******
+# def centered_star_pyramid(n):
+# #     return ""
+
