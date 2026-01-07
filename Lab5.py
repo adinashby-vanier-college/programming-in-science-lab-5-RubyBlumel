ccc5239b9edee609d8a56131a7e5254130679d62
RubyBlumel <rkblumel@gmail.com>
2026-01-07 14:14:00 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -27,22 +27,41 @@ def hollow_square(n):
 # # 12
 # # 123
 # # 1234
+
 def number_pattern(n):
+   
+   result = ""
+   for i in (range(1, (n + 1))):
+       for j in range(1, i + 1):
+          result += str(j)
+
+       result += "\n"
 
     
-    return result
 
-# # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
-def sum_of_natural_numbers(n):
-    return ""
 
-# # Example for n = 4:
-# #    *
-# #   ***
-# #  *****
-# # *******
-def centered_star_pyramid(n):
-    return ""
+   return result
+
+ 
+
+       
+      
+
+
+# # # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
+# def sum_of_natural_numbers(n):
+#     return ""
+
+# # # Example for n = 4:
+# # #    *
+# # #   ***
+# # #  *****
+# # # *******
+# def centered_star_pyramid(n):
+#     return ""
+
+# result = hollow_square(5)
+# print(result)
 
 result = number_pattern(4)
 print(result)
